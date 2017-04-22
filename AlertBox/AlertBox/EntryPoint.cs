70e7e8dfa8b6727dc79175b57b60347b7c795756
Plomboe <m.tareq.karimi@gmail.com>
2017-04-22 01:30:12 -0400

    Selenium Automation Testing
    Course by Tod Vachev on Udemy

---
+++ AlertBox/AlertBox/EntryPoint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    
+    //Can't use IWebElement, since it isn't an element. Can't find from "inspect element"
+    static IAlert alert;
+    
+    /* To see if you actually selected the textbox, select an element you can ONLY see after
+       the textbox has been clicked on */
+    static IWebElement image;
+
+
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/special-elements/alert-box/";
+
+        driver.Navigate().GoToUrl(url);
+
+        alert = driver.SwitchTo().Alert();
+
+        //This takes the text out of the alert box, to display the textbox text 
+        Console.WriteLine(alert.Text);
+
+        alert.Accept();
+
+        image = driver.FindElement(By.CssSelector("#post-119 > div > figure > img"));
+        try
+        {
+            if (image.Displayed)
+            {
+                Console.WriteLine("The alert was successfuly accepted and I can see the image!");
+            }
+        }
+        catch (NoSuchElementException)
+        {
+            Console.WriteLine("Something went wrong!!");
+        }
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
+
+    }
+}
+
